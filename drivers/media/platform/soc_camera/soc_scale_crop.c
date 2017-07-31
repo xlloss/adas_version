de4adb3655cb42935e27b2e2c5b11e9d9b93c8fb
Vladimir Barinov <vladimir.barinov@cogentembedded.com>
2017-07-31 19:26:05 +0300

    media: soc_camera: Fix VIDIOC_S_SELECTION ioctl miscalculation
    This patch corrects the miscalculation of the capture buffer
    size and clipping data update in VIDIOC_S_SELECTION sequence.
    
    Patch isbased on work by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>
    
    Signed-off-by: Vladimir Barinov <vladimir.barinov@cogentembedded.com>

--- drivers/media/platform/soc_camera/soc_scale_crop.c
+++ drivers/media/platform/soc_camera/soc_scale_crop.c
@@ -72,14 +72,14 @@ static void update_subrect(struct v4l2_rect *rect, struct v4l2_rect *subrect)
 
 	if (rect->left > subrect->left)
 		subrect->left = rect->left;
-	else if (rect->left + rect->width >
+	else if (rect->left + rect->width <
 		 subrect->left + subrect->width)
 		subrect->left = rect->left + rect->width -
 			subrect->width;
 
 	if (rect->top > subrect->top)
 		subrect->top = rect->top;
-	else if (rect->top + rect->height >
+	else if (rect->top + rect->height <
 		 subrect->top + subrect->height)
 		subrect->top = rect->top + rect->height -
 			subrect->height;
@@ -125,6 +125,7 @@ int soc_camera_client_s_selection(struct v4l2_subdev *sd,
 		dev_dbg(dev, "Camera S_SELECTION successful for %dx%d@%d:%d\n",
 			rect->width, rect->height, rect->left, rect->top);
 		*target_rect = *cam_rect;
+		*subrect = *rect;
 		return 0;
 	}
 
@@ -216,6 +217,7 @@ int soc_camera_client_s_selection(struct v4l2_subdev *sd,
 
 	if (!ret) {
 		*target_rect = *cam_rect;
+		*subrect = *rect;
 		update_subrect(target_rect, subrect);
 	}
 
@@ -296,9 +298,7 @@ update_cache:
 	if (ret < 0)
 		return ret;
 
-	if (host_1to1)
-		*subrect = *rect;
-	else
+	if (!host_1to1)
 		update_subrect(rect, subrect);
 
 	return 0;
